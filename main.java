8edfd5059d7439b100a321a5ba42455194103c78
TusharPrasad12 <tusharprasad200318@gmail.com>
2024-08-18 22:24:05 +0530

    main_function

---
+++ main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Ecexutor;
+
+public clas Main extends JApplet {
+    public static dinal long serialVersionUID = 1L;
+    private SortPanel[] sortPanels = new SortPanel[9];
+    private static int size = 100;
+    private int SleepTime=2;
+    private String animationName="";
+
+    public Main(){
+        setLayout(new GridLayout(1,1,0,0));
+        SortPanelsHolder sortPanelsHolder = new SortPanelsHolder();
+        sortPanelsHolder.setLayout
+    }
+
+}
